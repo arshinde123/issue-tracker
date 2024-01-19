fb652d9b90f3e6c2b9384360ce6e7b1b6b69c029
Arvind <arshinde123.2013@gmail.com>
2024-01-19 12:30:52 +0530

    Customizing radix ui theme

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -28,9 +28,9 @@ const NavBar = () => {
           <li key={link.href}>
             <Link
               className={classNames({
-                "text-zinc-900": link.href === path,
+                "text-zinc-900 bg-slate-100 px-5 py-2": link.href === path,
                 "text-zinc-500": link.href !== path,
-                "hover:text-zinc-800 transition-colors": true,
+                "hover:text-zinc-800 transition-all": true,
               })}
               href={link.href}
             >
