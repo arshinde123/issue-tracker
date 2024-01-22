2c979d32e25117b4ae477682b9cd2a701efa2245
Arvind <arshinde123.2013@gmail.com>
2024-01-22 13:54:29 +0530

    Show confirmation dialog

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { issueSchema } from "../../validationSchemas";
+import OpenAI from "openai";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -17,4 +18,34 @@ export async function POST(request: NextRequest) {
   });
 
   return NextResponse.json(newIssue, { status: 201 });
+}
+
+export async function PUT(request: NextRequest) {
+  const body = await request.json();
+  console.log({ body });
+  console.log(`*******************${body.prompt}*******************`);
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const response = await openai.completions.create({
+    model: "gpt-3.5-turbo-instruct",
+    // model: "babbage-002",
+    // model: "davinci-002",
+    // messages: [
+    //   {
+    //     role: "user",
+    //     content:
+    //       "Write summary for 6 year experienced frontend engineer for resume",
+    //   },
+    // ],
+    prompt: body.prompt,
+    temperature: 1,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return NextResponse.json(response.choices[0].text);
 }
