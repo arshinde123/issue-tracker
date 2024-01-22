b73c3bd8ed6d12780402cd5cd290d655f8aa8572
Arvind <arshinde123.2013@gmail.com>
2024-01-22 17:02:09 +0530

    Change NavBar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const path = usePathname();
@@ -30,36 +30,39 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900 bg-slate-100 rounded px-5 py-2": showActiveLink(
-                  link.href
-                ),
-                "text-zinc-500": showActiveLink(link.href),
-                "hover:text-zinc-800 transition-all": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log Out</Link>
-        )}
-      </Box>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Flex justify="between">
+        <Flex align="center" gap="3">
+          <Link href="/">
+            <AiFillBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classNames({
+                    "text-zinc-900 bg-slate-100 rounded px-5 py-2":
+                      showActiveLink(link.href),
+                    "text-zinc-500": showActiveLink(link.href),
+                    "hover:text-zinc-800 transition-all": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Box>
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
+          {status === "authenticated" && (
+            <Link href="/api/auth/signout">Log Out</Link>
+          )}
+        </Box>
+      </Flex>
     </nav>
   );
 };
