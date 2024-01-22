5a9650315ea89fe06402596c057b2946e121f403
Arvind <arshinde123.2013@gmail.com>
2024-01-22 15:36:58 +0530

    Restructure folders to avoid duplicate loading skeleton

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -39,7 +39,7 @@ const IssueForm = async ({ issue }: Props) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues/", data);
 
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setSubmitting(false);
